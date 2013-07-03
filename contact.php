b2c76f0261ff732d186cf8a9d5ff451479d9803d
John Costallos <jpcostallos@gmail.com>
2013-07-03 11:51:37 -0400

    modify header hierarchy

--- contact.php
+++ contact.php
@@ -13,23 +13,23 @@ get_header(); ?>
 				
 
 				<section class="contact">
-					<h1>Contact Us</h1>
-					<h2>Mailing Address:</h2>
+					<h2>Contact Us</h2>
+					<h3>Mailing Address:</h3>
 					<p>Knights of the RoundTable
 						University of Central Florida
 						12715 Pegasus Dr.
 						Student Union, Room 208
 						Orlando, FL 32816 </p>
-					<h2>Directions:</h2>
+					<h3>Directions:</h3>
 					<p>KoRT is located on the second floor of the Student Union, inside the Office of Student Involvement, Room 208.</p>
-					<h2>Phone:</h2>
+					<h3>Phone:</h3>
 					<a href="tel:407-823-4496">407-823-4496</a>
-					<h2>Fax:</h2>
+					<h3>Fax:</h3>
 					<a href="tel:407-823-5899">407-823-5899</a>
-					<h2>Email:</h2>
+					<h3>Email:</h3>
 					<a href="mailto:kort@ucf.edu">kort@ucf.edu</a>
 					<div>
-						<h2>Social:</h2>
+						<h3>Social:</h3>
 						<a class="facebook" href="#">Facebook</a>
 						<a class="twitter" href="#">Twitter</a>
 					</div>	
@@ -40,7 +40,7 @@ get_header(); ?>
 
 
 				<section class="leader-board">
-					<h1>Executive Board</h1>
+					<h2>Executive Board</h2>
 <?php
 					$leaderLoop = new WP_QUERY(array('post_type' => 'leader-board', 'posts_per_page' => -1, 'orderby' =>'meta_value', 'order' => 'ASC', 'meta_key' => 'leader-form-order'));
 					while ($leaderLoop->have_posts()) {
@@ -53,7 +53,7 @@ get_header(); ?>
 ?>	
 					<article class="leader-board">
 						<?php echo $image; ?>
-						<h2><?php echo $title; ?></h2>
+						<h3><?php echo $title; ?></h3>
 						<p><?php echo $position; ?><p>
 						<p><?php echo $content; ?><p>
 						<a class="email" href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
