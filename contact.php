784543b621dcf53dbfe0dc8b8dcafc9244e97edd
John Costallos <jpcostallos@gmail.com>
2013-07-09 14:52:17 -0400

    correct stray php tags

--- contact.php
+++ contact.php
@@ -13,26 +13,30 @@ get_header(); ?>
 				
 
 				<section class="contact">
-					<h2>Contact Us</h2>
-					<h3>Mailing Address:</h3>
-					<p>Knights of the RoundTable
-						University of Central Florida
-						12715 Pegasus Dr.
-						Student Union, Room 208
-						Orlando, FL 32816 </p>
-					<h3>Directions:</h3>
-					<p>KoRT is located on the second floor of the Student Union, inside the Office of Student Involvement, Room 208.</p>
-					<h3>Phone:</h3>
-					<a href="tel:407-823-4496">407-823-4496</a>
-					<h3>Fax:</h3>
-					<a href="tel:407-823-5899">407-823-5899</a>
-					<h3>Email:</h3>
-					<a href="mailto:kort@ucf.edu">kort@ucf.edu</a>
-					<div>
+					<article class="address">
+						<h2>Contact Us</h2>
+						<h3>Mailing Address:</h3>
+						<p>Knights of the RoundTable
+							University of Central Florida
+							12715 Pegasus Dr.
+							Student Union, Room 208
+							Orlando, FL 32816 </p>
+						<h3>Directions:</h3>
+						<p>KoRT is located on the second floor of the Student Union, inside the Office of Student Involvement, Room 208.</p>
+					</article>
+					<article class="contact">
+						<h3>Phone:</h3>
+						<a href="tel:407-823-4496">407-823-4496</a>
+						<h3>Fax:</h3>
+						<a href="tel:407-823-5899">407-823-5899</a>
+						<h3>Email:</h3>
+						<a href="mailto:kort@ucf.edu">kort@ucf.edu</a>
+					</article>
+					<article class="social">
 						<h3>Social:</h3>
 						<a class="facebook" href="#">Facebook</a>
 						<a class="twitter" href="#">Twitter</a>
-					</div>	
+					</article>	
 				</section>
 
 
@@ -58,8 +62,8 @@ get_header(); ?>
 						<p><?php echo $content; ?><p>
 						<a class="email" href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
 					</article>
-				}
-
+<?php 				}
+?>
 				</section> 
 			
 			</div>
