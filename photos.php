44303bfc2762e504355e8a5b00a413fae88b3f2b
John Costallos <jpcostallos@gmail.com>
2013-07-11 18:02:01 -0400

    create photos.php

---
+++ photos.php
@@ -0,0 +1,35 @@
+<?php /* Template Name: Photos */
+
+get_header(); ?>
+
+			<div class="content-area">
+				<div class="main"> 
+					<?php
+					while (have_posts()) {
+						the_post();
+						get_template_part( 'content', 'page' );
+					} ?>
+				</div>
+
+				<section class="photos">
+						<h2>Photo Albums</h2>
+<?php
+						$albumloop = new WP_QUERY(array('post_type' => 'fb-albums', 'posts_per_page' => -1, 'orderby' =>'meta_value', 'order' => 'date'));
+						while ($albumloop->have_posts()) {
+							$albumloop->the_post();
+							$title = get_the_title();
+							$albumid = get_post_meta($post->ID 'album-form-id', true);
+							$albumlink = get_post_meta($post->ID 'album-form-link', true);
+?>	
+						<article class="album">
+							<h3><?php echo $title; ?></h3>
+							<?php echo do_shortcode('fb id=' . $albumid . ' limit=6 rand=1'); ?>
+							<a href="<?php echo $albumlink; ?>">See More on Facebook</a>
+						</article>
+<?php 				}
+?>
+					</section> 
+			
+			</div>
+
+<?php get_footer(); ?>
