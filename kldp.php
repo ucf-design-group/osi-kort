1f8f2c2a2d328ff5d627a08d5e772eb229f4a028
John Costallos <jpcostallos@gmail.com>
2013-07-15 13:21:11 -0400

    add counter to Board of Directors h2

--- kldp.php
+++ kldp.php
@@ -12,21 +12,30 @@ get_header(); ?>
 				</div>
 
 				<section class="kldp-board">
-					<h2>Board of Directors</h2>
+					
 
 <?php
+					$counter = 0;
+
 					$leaderLoop = new WP_QUERY(array('post_type' => 'kldp-board', 'posts_per_page' => -1, 'orderby' =>'meta_value', 'order' => 'ASC', 'meta_key' => 'kldp-form-order'));
 					while ($leaderLoop->have_posts()) {
 						$leaderLoop->the_post();
 						$title = get_the_title();
 						$content = get_the_content();
 						$image = get_the_post_thumbnail($post->ID, 'small');
+
+						if ($counter==0){
+?>
+					<h2>Board of Directors</h2>
+<?php
+						}
 ?>	
 					<article class="kldp-leader">
 						<?php echo $image; ?>
 						<h3><?php echo $title; ?></h3>
 					</article>
 <?php
+					$counter++;
 				}
 ?>
 				</section> 
